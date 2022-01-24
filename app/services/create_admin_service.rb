ab8d4e9b382ada220c3b4c7f4488165642299458
Artur Masagutov <masagutovartur765@gmail.com>
2022-01-25 00:43:40 +0500

    Fix seeds

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,8 +1,5 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
-      end
+    user = User.find_or_create_by!(email: "test@example.com", password: "test123")
   end
 end
