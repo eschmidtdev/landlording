b4648f2367ec624ec74156af4287402e7c97a949
Artur Masagutov <masagutovartur765@gmail.com>
2022-02-03 02:00:16 +0500

    Working on login forms

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,5 +1,5 @@
 class CreateAdminService
   def call
-    user = User.create!(email: "test@example.com", password: "test123")
+    User.create!(email: "test@example.com", password: "test123")
   end
 end
