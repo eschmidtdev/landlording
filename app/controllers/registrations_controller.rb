8ab5b26190a7a9d32e4e0605ca8e4388ded53006
Artur Masagutov <masagutovartur765@gmail.com>
2022-02-05 01:54:08 +0500

    Fix for verify auth

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,17 @@
+# frozen_string_literal: true
+
+# This controller inherits from Devise::RegistrationsController & for Users Registrations
 class RegistrationsController < Devise::RegistrationsController
-  def new
-    super
-  end
+  skip_before_action :verify_authenticity_token
 
   def create
     return if params_missing?(params[:user]) || user_exists?(params[:user]) ||
-      password_clashing?(params[:user][:password], params[:user][:password_confirmation])
+              password_clashing?(params[:user][:password], params[:user][:password_confirmation])
 
     resource = User.new(user_params)
     resource.save
-    set_flash_message(:registrations, :signed_up, options = {})
+    set_flash_message(:registrations, :signed_up)
     redirect_to root_path
-  end
-
-  def update
-    super
   end
 
   private
@@ -27,26 +24,25 @@ class RegistrationsController < Devise::RegistrationsController
   def hash_has_blank(hash)
     hash.values.any? do |i|
       if i.empty?
-        set_flash_message(:registrations, :missing_params, options = {})
+        set_flash_message(:registrations, :missing_params)
         redirect_to root_path
       end
     end
-
   end
 
   def user_exists?(params)
     if User.exists?(email: params[:email])
-      set_flash_message(:registrations, :email_taken, options = {})
+      set_flash_message(:registrations, :email_taken)
       redirect_to root_path
     end
   end
 
   def password_clashing?(password, confirm_password)
     if password.length < set_minimum_password_length
-      set_flash_message(:passwords, :min_length, options = {})
+      set_flash_message(:passwords, :min_length)
       redirect_to root_path
     elsif password != confirm_password
-      set_flash_message(:passwords, :invalid_confirmation, options = {})
+      set_flash_message(:passwords, :invalid_confirmation)
       redirect_to root_path
     end
   end
@@ -54,5 +50,4 @@ class RegistrationsController < Devise::RegistrationsController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
 end
