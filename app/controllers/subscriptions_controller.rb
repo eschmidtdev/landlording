974e3c4462d6eb312ec26a81f1781c1fb7ffd713
Artur Masagutov <masagutovartur765@gmail.com>
2022-02-15 22:11:44 +0500

    Create plans page

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,26 +1,27 @@
+# frozen_string_literal: true
+
+# This controller is responsible for manage subscription
 class SubscriptionsController < ApplicationController
-  before_action :set_subscription, only: %i[ show edit update destroy ]
+  before_action :set_subscription, only: %i[show edit update destroy]
 
   def index
     @subscriptions = Subscription.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @subscription = Subscription.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @subscription = Subscription.new(subscription_params)
 
     respond_to do |format|
       if @subscription.save
-        format.html { redirect_to subscription_url(@subscription), notice: "Subscription was successfully created." }
+        format.html { redirect_to subscription_url(@subscription), notice: 'Subscription was successfully created.' }
         format.json { render :show, status: :created, location: @subscription }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -32,7 +33,7 @@ class SubscriptionsController < ApplicationController
   def update
     respond_to do |format|
       if @subscription.update(subscription_params)
-        format.html { redirect_to subscription_url(@subscription), notice: "Subscription was successfully updated." }
+        format.html { redirect_to subscription_url(@subscription), notice: 'Subscription was successfully updated.' }
         format.json { render :show, status: :ok, location: @subscription }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -45,10 +46,12 @@ class SubscriptionsController < ApplicationController
     @subscription.destroy
 
     respond_to do |format|
-      format.html { redirect_to subscriptions_url, notice: "Subscription was successfully destroyed." }
+      format.html { redirect_to subscriptions_url, notice: 'Subscription was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
+
+  def plans; end
 
   private
 
@@ -57,6 +60,7 @@ class SubscriptionsController < ApplicationController
   end
 
   def subscription_params
-    params.require(:subscription).permit(:email, :first_name, :last_name, :company, :address_line_one, :address_line_two, :city, :zip_code, :country, :state)
+    params.require(:subscription).permit(:email, :first_name, :last_name, :company, :address_line_one,
+                                         :address_line_two, :city, :zip_code, :country, :state)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :subscriptions
-  resources :e_forms
   root to: 'visitors#index'
   devise_for :users, controllers: { registrations: "registrations", sessions: "sessions" }
   resources :users
+  resources :e_forms
+  resources :subscriptions do
+    collection do
+      get :plans
+    end
+  end
 
 end
