2c232b3bad5d97e942f80d38744c4c1097336ae1
Artur Masagutov <masagutovartur765@gmail.com>
2022-02-18 21:59:35 +0500

    Responsivness for Logins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def from_e_form_tab?
+    current_page?(e_forms_path) ||
+    current_page?(edit_user_registration_path) ||
+    current_page?(subscriptions_path) ||
+    current_page?(root_path)
+  end
 end
