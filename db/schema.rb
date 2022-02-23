be301c18b0b0be4b2bff61f660a231f74221d635
Artur Masagutov <masagutovartur765@gmail.com>
2022-02-24 00:40:42 +0500

    Add User Id to subscription

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_171154) do
+ActiveRecord::Schema.define(version: 2022_02_23_184238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2022_02_17_171154) do
     t.string "state"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
