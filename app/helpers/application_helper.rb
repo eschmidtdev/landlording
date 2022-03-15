8bd01f08127a2e178f8a758dee295140cedf4c9c
Artur Masagutov <masagutovartur765@gmail.com>
2022-03-15 23:27:13 +0500

    Fixed for Active eform tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,11 @@
 # This module is responsible for method that is (mostly) used in your Rails views to share reusable code
 module ApplicationHelper
   def from_e_form_tab?
-    current_page?(documents_path) ||
-      current_page?(edit_user_registration_path) ||
-      current_page?(subscriptions_path) ||
-      current_page?(root_path)
+      params[:controller] == 'subscriptions' ||
+      params[:controller] == 'documents' ||
+      params[:controller] == 'payments' ||
+      params[:controller] == 'registrations' ||
+      params[:controller] == 'visitors'
   end
 
   def check_for(value)
