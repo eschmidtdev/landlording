ddfa2964bc6fdd57c54f9500176724329758a1b8
Artur Masagutov <masagutovartur765@gmail.com>
2022-03-17 22:11:22 +0500

    Fixed Document Update Issues

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# This Controller is responsible for handling index actions
 class VisitorsController < ApplicationController
   def index
     redirect_to documents_path if user_signed_in?
