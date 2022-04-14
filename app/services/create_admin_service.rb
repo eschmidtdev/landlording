6960ee03ed24c5a4de0bcce33fd405ad878c6baa
Artur Masagutov <masagutovartur765@gmail.com>
2022-04-14 16:50:10 +0500

    Fixed seeds

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -2,13 +2,12 @@
 
 # Service for create Admin User
 class CreateAdminService
-  attr_reader :name, :email, :pass, :token, :current_datetime
 
-  def initialize(name, email, pass, token, current_datetime)
+  def initialize(name, email, password, confirmation_token, current_datetime)
     @name               = name
     @email              = email
-    @pass               = pass
-    @confirmation_token = token
+    @password           = password
+    @confirmation_token = confirmation_token
     @datetime           = current_datetime
   end
 
@@ -16,13 +15,14 @@ class CreateAdminService
     prev_user = User.find_by email: @email
     raise StandardError, I18n.t('EForm.Messages.Error.AlreadyExists') if prev_user.present?
 
-    User.create(
+    user = User.create(
       name: @name,
       email: @email,
-      password: @pass,
+      password: @password,
       confirmed_at: @datetime,
       confirmation_sent_at: @datetime,
       confirmation_token: @confirmation_token
     )
+    user.save!
   end
 end
