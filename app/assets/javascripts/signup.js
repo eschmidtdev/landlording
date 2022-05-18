19adbcf8d53fae436349fdc4018898e9dee50139
Artur Masagutov <masagutovartur765@gmail.com>
2022-05-18 14:11:05 +0500

    Added Form Validation Jquery

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-    $("#RegistrationForm").validate();
+    $(function () {
+        $("form[name='registration']").validate({
+            rules: {
+                'user[name]': {required: true},
+                'user[email]': {required: true, email: true},
+                'user[password]': {required: true, minlength: 8}
+            },
+            messages: {
+                'user[name]': {
+                    required: "Full Name is required.",
+                },
+                'user[email]': "Please enter a valid email address",
+                'user[password]': {
+                    required: "Please provide a password",
+                    minlength: "Password is too short (minimum is 8 characters)"
+                }
+            },
+            submitHandler: function (form) {
+                form.submit();
+            }
+        });
+    });
+
 });
