02d9c463c24847fd6268fc8fbc67e89d7d861f21
Artur Masagutov <masagutovartur765@gmail.com>
2022-05-18 13:05:49 +0500

    Added Validation Plugin & Changed Ids

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require jquery.validate
+//= require jquery.validate.additional-methods
 //= require_tree .
 
 // Auto hide notification div
