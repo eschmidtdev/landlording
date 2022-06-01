9da86e6553e007f4c7c1bb5f33381da3d896dd18
Artur Masagutov <masagutovartur765@gmail.com>
2022-06-01 19:59:49 +0500

    Added before_action to prevent direct access to url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-# Responsible for handling HTTP request that enters into application's
-# codebase from the client
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, unless: :devise_controller?
 
   protected
 
@@ -12,4 +12,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
 end
