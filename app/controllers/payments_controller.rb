e57c5aba5f2f4fc8ff06821a5d45c3096c4757e6
Artur Masagutov <masagutovartur765@gmail.com>
2022-06-01 14:26:52 +0500

    Working on buy plan page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,16 +1,7 @@
 # frozen_string_literal: true
 
 class PaymentsController < ApplicationController
-  before_action :set_subscription, only: :index
 
-  def index
-    @subscription
-    @countries = CS.countries.values.sort
-  end
+  def index; end
 
-  private
-
-  def set_subscription
-    @subscription = Subscription.find(params[:id])
-  end
 end
