7bf315c14bb08ee06137b890eaf0fb4d6b337c00
Artur Masagutov <masagutovartur765@gmail.com>
2022-06-20 19:00:31 +0500

    Dynamic card details

--- app/helpers/account_settings_helper.rb
+++ app/helpers/account_settings_helper.rb
@@ -1,4 +1,28 @@
 # frozen_string_literal: true
 
 module AccountSettingsHelper
+
+  def card_number(user)
+    if user.payment_detail.present?
+      "************#{user.payment_detail&.card_number&.last(4)}"
+    else
+      '****************'
+    end
+  end
+
+  def expiry_date(user)
+    if user.payment_detail.present?
+      "Expires #{user.payment_detail&.exp&.first(2)}/#{user.payment_detail&.exp&.last(2)}"
+    else
+      'Expires 00/00'
+    end
+  end
+
+  def billing_address(user)
+    if user.payment_detail.present?
+      user.payment_detail&.address_line_one + user.payment_detail&.address_line_two
+    else
+      '123 Apple Street, Unit 101, Austin, TX 78741'
+    end
+  end
 end
