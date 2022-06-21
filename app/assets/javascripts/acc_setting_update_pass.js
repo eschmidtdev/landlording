2c2067d2110cee03ab9d5ca61b768b183f5de3ac
Artur Masagutov <masagutovartur765@gmail.com>
2022-06-21 15:56:37 +0500

    Working on Responsiveness

--- app/assets/javascripts/acc_setting_update_pass.js
+++ app/assets/javascripts/acc_setting_update_pass.js
@@ -3,17 +3,17 @@ $(document).ready(function () {
     $(function () {
         $("form[name='account_change_password']").validate({
             rules: {
-                'Setting[CP]':  {required: true, minlength: 8},
-                'Setting[NP]':  {required: true, minlength: 8},
+                'Setting[CP]': {required: true, minlength: 8},
+                'Setting[NP]': {required: true, minlength: 8},
                 'Setting[CNP]': {required: true, equalTo: "#SettingNP"},
             },
             messages: {
                 'Setting[CP]': {
-                    required:  'Current Password is required',
+                    required: 'Current Password is required',
                     minlength: 'Password is too short (minimum is 8 characters)'
                 },
                 'Setting[NP]': {
-                    required:  'New Password is required',
+                    required: 'New Password is required',
                     minlength: 'Password is too short (minimum is 8 characters)'
                 },
                 'Setting[CNP]': {required: 'Confirm New Password is required'},
@@ -27,19 +27,19 @@ $(document).ready(function () {
     function ajaxReqUpdatePassword(e) {
         e.preventDefault();
         disableButton();
-        const new_password     = $('#SettingNP').val();
+        const new_password = $('#SettingNP').val();
         const current_password = $('#SettingCP').val();
         const confirm_password = $('#SettingCNP').val();
-        const user_id          = $('#AccSettingPassUpdateUID').val();
+        const user_id = $('#AccSettingPassUpdateUID').val();
         $.ajax({
             url: `/account_settings/${user_id}`,
             type: 'PUT',
             data: {
                 account_setting: {
-                    new_password:     new_password,
+                    new_password: new_password,
+                    from: 'ChangePassword',
                     current_password: current_password,
-                    confirm_password: confirm_password,
-                    from:             'ChangePassword'
+                    confirm_password: confirm_password
                 }
             },
             success: function (data) {
@@ -72,7 +72,8 @@ $(document).ready(function () {
 
     function render_response(data) {
         $("#ChangePassword").trigger("reset");
-        $('.error_alert')
+        $('.success_alert')
+            .text('')
             .show()
             .append(data.message)
             .delay(2000)
@@ -81,17 +82,22 @@ $(document).ready(function () {
 
     function render_conditional_response(data) {
         if (data.error === 'current_password') {
-            $('#PassIncorrect').show().text(data.message);
+            $('#PassIncorrect').text('').show().text(data.message);
         }
     }
 
     function clearErrors() {
         $('.error_alert').text('').addClass('display_none');
+        $('.success_alert').text('').addClass('display_none');
         $('#PassIncorrect').text('').addClass('display_none');
     }
 
     $('#SettingCP').keyup(function () {
         $('#PassIncorrect').hide();
     });
+
+    $(":input").on("keyup change", function (e) {
+        clearErrors();
+    })
 
 });
