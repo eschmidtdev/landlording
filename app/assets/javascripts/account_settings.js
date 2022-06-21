4423f109ecb3c77c2e93d34e3a198d65fd543c14
Artur Masagutov <masagutovartur765@gmail.com>
2022-06-21 13:57:34 +0500

    Showed corrct alert color

--- app/assets/javascripts/account_settings.js
+++ app/assets/javascripts/account_settings.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
     $(function () {
         $("form[name='personal_info']").validate({
             rules: {
-                'accountSetting[LastName]':    {required: true},
-                'accountSetting[FirstName]':   {required: true},
+                'accountSetting[LastName]': {required: true},
+                'accountSetting[FirstName]': {required: true},
                 'accountSetting[PhoneNumber]': {required: true},
             },
             messages: {
-                'accountSetting[LastName]':    'Last Name is required',
-                'accountSetting[FirstName]':   'First Name is required',
+                'accountSetting[LastName]': 'Last Name is required',
+                'accountSetting[FirstName]': 'First Name is required',
                 'accountSetting[PhoneNumber]': 'Phone Number is required'
             },
             submitHandler: function (form, e) {
@@ -18,7 +18,7 @@ $(document).ready(function () {
         });
     });
 
-    $( "#DeleteBillingDetails" ).click(function() {
+    $("#DeleteBillingDetails").click(function () {
         const userID = $(this).data('user-id');
         ajaxDeleteReq(userID);
     });
@@ -26,18 +26,18 @@ $(document).ready(function () {
     function ajaxRequest(e) {
         e.preventDefault();
         disableButton();
-        const last_name    = $('#accountSettingLastName').val();
-        const first_name   = $('#accountSettingFirstName').val();
+        const last_name = $('#accountSettingLastName').val();
+        const first_name = $('#accountSettingFirstName').val();
         const phone_number = $('#accountSettingPhoneNumber').val();
         const company_name = $('#accountSettingCompanyName').val();
-        const user_id      = $('#accountSettingUserID').val();
+        const user_id = $('#accountSettingUserID').val();
         $.ajax({
             url: `/account_settings/${user_id}`,
             type: 'PUT',
             data: {
                 account_setting: {
-                    last_name:    last_name,
-                    first_name:   first_name,
+                    last_name: last_name,
+                    first_name: first_name,
                     phone_number: phone_number,
                     company_name: company_name
                 }
@@ -91,7 +91,8 @@ $(document).ready(function () {
 
     function render_message(data) {
         clearErrors();
-        $('.error_alert')
+        $(data.success === true ? '.success_alert' : '.error_alert')
+            .text('')
             .show()
             .append(data.message)
             .delay(2000)
