03d1288700bd3e637785abf3d15c769b8795da5e
Artur Masagutov <masagutovartur765@gmail.com>
2022-07-15 05:41:24 -0700

    Added Comment to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@ CreateAdminService.call('Autr', 'Mesagutov',
 CreateAdminService.call('Autr', 'Mesagutov',
                         'artur@ipm.com', 'Test@123',
                         SecureRandom.hex(10), DateTime.now)
+
+After running seed file run following command
+
+#  1- For development env
+     #  rake zipcodes:update
+#  2- For production env(heroku)
+     #  heroku rake zipcodes:update
