1c453ae95d36bd2ca8e39be119e989e95d6748a3
Artur Masagutov <masagutovartur765@gmail.com>
2022-08-19 03:20:12 -0700

    Added Account Setting Validators

--- app/assets/javascripts/account_settings.js
+++ app/assets/javascripts/account_settings.js
@@ -3,14 +3,24 @@ $(document).ready(function () {
     $(function () {
         $("form[name='personal_info']").validate({
             rules: {
-                'accountSetting[LastName]': {required: true},
-                'accountSetting[FirstName]': {required: true},
-                'accountSetting[PhoneNumber]': {required: true},
+                'accountSetting[City]':         {required: true},
+                'accountSetting[State]':        {required: true},
+                'accountSetting[Country]':      {required: true},
+                'accountSetting[LastName]':     {required: true},
+                'accountSetting[FirstName]':    {required: true},
+                'accountSetting[PostalCode]':   {required: true},
+                'accountSetting[PhoneNumber]':  {required: true},
+                'accountSetting[AddressLine1]': {required: true}
             },
             messages: {
-                'accountSetting[LastName]': 'Last Name is required',
-                'accountSetting[FirstName]': 'First Name is required',
-                'accountSetting[PhoneNumber]': 'Phone Number is required'
+                'accountSetting[City]':         'City is required',
+                'accountSetting[State]':        'State is required',
+                'accountSetting[Country]':      'Country is required',
+                'accountSetting[LastName]':     'Last Name is required',
+                'accountSetting[FirstName]':    'First Name is required',
+                'accountSetting[PostalCode]':   'Postal Code is required',
+                'accountSetting[PhoneNumber]':  'Phone Number is required',
+                'accountSetting[AddressLine1]': 'Address Line 1 is required'
             },
             submitHandler: function (form, e) {
                 ajaxRequest(e);
@@ -34,18 +44,26 @@ $(document).ready(function () {
     function ajaxRequest(e) {
         e.preventDefault();
         disableButton();
-        const user_id = $('#accountSettingUserID').val();
-        const last_name = $('#accountSettingLastName').val();
-        const first_name = $('#accountSettingFirstName').val();
+        const city         = $('#accountSettingCity').val();
+        const state        = $('#accountSettingState').val();
+        const user_id      = $('#accountSettingUserID').val();
+        const country      = $('#accountSettingCountry').val();
+        const last_name    = $('#accountSettingLastName').val();
+        const first_name   = $('#accountSettingFirstName').val();
         const phone_number = $('#accountSettingPhoneNumber').val();
         const company_name = $('#accountSettingCompanyName').val();
+        const postal_code  = $('#accountSettingPostalCode').val();
         $.ajax({
             url: `/account_settings/${user_id}`,
             type: 'PUT',
             data: {
                 account_setting: {
-                    last_name: last_name,
-                    first_name: first_name,
+                    city:         city,
+                    state:        state,
+                    country:      country,
+                    last_name:    last_name,
+                    first_name:   first_name,
+                    postal_code:  postal_code,
                     phone_number: phone_number,
                     company_name: company_name
                 }
--- app/controllers/account_settings_controller.rb
+++ app/controllers/account_settings_controller.rb
@@ -2,6 +2,7 @@
 
 class AccountSettingsController < ApplicationController
   before_action :set_user, only: %i[update destroy]
+  before_action :validate_account_setting, only: :update
 
   include Responseable
 
@@ -14,9 +15,6 @@ class AccountSettingsController < ApplicationController
   def change_password; end
 
   def update
-    response = AccountSettingsValidatorService.call(account_settings_params.to_h, @user)
-    return render_response(false, response[:message], response[:method], nil) unless response.nil?
-
     resp = AccountSettingsUpdateService.call(account_settings_params.to_h, @user)
     render_response(resp[:success], resp[:message], nil, nil)
   end
@@ -31,15 +29,20 @@ class AccountSettingsController < ApplicationController
   def set_user = @user = User.find(params[:id])
 
   def account_settings_params
-    params.require(:account_setting).permit(required_params)
-  end
-
-  def required_params
-    User.column_names.reject { |k| [' id '].include?(k) }.map(&:to_sym) + (update_pass_params)
+    params.require(:account_setting).permit(User.column_names.reject { |k| [' id '].include?(k) }.map(&:to_sym) + (update_pass_params))
   end
 
   def update_pass_params
     %i[new_password from current_password confirm_password]
+  end
+
+  def validate_account_setting
+    response = Validators::AccountSettingsValidatorService.call(params[:action], account_settings_params.to_h, @user)
+    unless response.nil?
+      flash[:notice] = response[:message]
+      redirect_to account_path
+    end
+
   end
 
 end
