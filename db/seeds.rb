ab5c5891e5da1d63f7bc6a517a2a12330690b809
Artur Masagutov <masagutovartur765@gmail.com>
2022-08-23 02:21:54 -0700

    Invoke Rake Task for zip codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
 # Create Admin User
-CreateAdminService.call('Autr', 'Mesagutov',
-                        'autr@ipm.com', 'Test@123',
-                        SecureRandom.hex(10), DateTime.now)
-CreateAdminService.call('Autr', 'Mesagutov',
-                        'artur@ipm.com', 'Test@123',
-                        SecureRandom.hex(10), DateTime.now)
+CreateAdminService.call('Autr', 'Mesagutov', 'autr@ipm.com', 'Test@123', SecureRandom.hex(10), DateTime.now)
 
-#After running seed file run following command
+CreateAdminService.call('Autr', 'Mesagutov', 'artur@ipm.com', 'Test@123', SecureRandom.hex(10), DateTime.now)
 
-#  1- For development env
-     #  rake zipcodes:update
-#  2- For production env(heroku)
-     #  heroku rake zipcodes:update
+Rake::Task['zipcodes:update'].invoke
