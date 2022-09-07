aaeedec476975c284ae37cdc70e5b4c2983c89a2
Artur Masagutov <masagutovartur765@gmail.com>
2022-09-07 22:51:45 +0500

    Added UUID to seperate admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,10 @@ first_name = 'Autr'
 phone = '2012987481'
 password = 'Test@123'
 last_name = 'Mesagutov'
-uuid = SecureRandom.uuid
 token = SecureRandom.hex(10)
 
-CreateAdminService.call(first_name, last_name, 'autr@ipm.com', password, token, DateTime.now, phone, uuid)
+CreateAdminService.call(first_name, last_name, 'autr@ipm.com', password, token, DateTime.now, phone, SecureRandom.uuid)
 
-CreateAdminService.call(first_name, last_name, 'artur@ipm.com', password, token, DateTime.now, phone, uuid)
+CreateAdminService.call(first_name, last_name, 'artur@ipm.com', password, token, DateTime.now, phone, SecureRandom.uuid)
 
 Rake::Task['zipcodes:update'].invoke
