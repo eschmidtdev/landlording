f2efc9bc798bcabfbebb3c6dfbf2e01eaddc1590
Artur Masagutov <masagutovartur765@gmail.com>
2022-09-09 11:45:58 +0500

    Removed button from create document

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 first_name = 'Autr'
-phone = '2012987481'
-password = 'Test@123'
-last_name = 'Mesagutov'
-token = SecureRandom.hex(10)
+password   = 'Test@123'
+last_name  = 'Mesagutov'
+phone      = '2012987481'
+token      = SecureRandom.hex(10)
 
 CreateAdminService.call(first_name, last_name, 'autr@ipm.com', password, token, DateTime.now, phone, SecureRandom.uuid)
 
