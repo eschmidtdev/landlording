2fa510a11bd69e8024ca208d9072faa087fca518
Artur Masagutov <masagutovartur765@gmail.com>
2022-09-28 12:31:48 +0500

    Logout from session after 15 minutes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
   # Associations
   has_one :subscription, dependent: :destroy
