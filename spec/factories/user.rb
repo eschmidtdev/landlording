80282e354d9bcb8705b9baf90def16dde19088a3
Artur Masagutov <masagutovartur765@gmail.com>
2022-10-19 15:52:20 +0500

    Write User Private Method Spec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :user do
     sign_in_count { 3 }
+    phone_number  { '5551115555' }
     email         { Faker::Internet.email }
     last_name     { Faker::Name.last_name }
     first_name    { Faker::Name.first_name }
--- spec/models/user.rb
+++ spec/models/user.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe(User, type: :model) do
+
   describe 'Associations' do
     context "The 'have_one' matcher" do
       it { should have_one(:subscription).dependent(:destroy) }
@@ -26,7 +27,34 @@ RSpec.describe(User, type: :model) do
     it 'Should test not_confirmed? & return true' do
       user = User.new(confirmed_at: nil)
       response = user.not_confirmed?
-      expect(response).to be_truthy
+      expect(response).to(be_truthy)
+    end
+  end
+
+  describe 'Private Methods/Activerecord::Callbacks' do
+    let(:user) { create(:user) }
+    context 'While Testing Subscription Association' do
+      it 'Should return true/ActiveRecord::Object' do
+        expect(user.subscription).not_to(be_falsey)
+      end
+    end
+
+    context 'While Testing PaymentDetail Association' do
+      it 'Should return true/ActiveRecord::Object' do
+        expect(user.payment_detail).not_to(be_falsey)
+      end
+    end
+
+    context 'While Testing normalize_phone Method/Activerecord::Callbacks' do
+      it 'Should return phone number with + format' do
+        expect(user.phone_number).to(eq('+555551115555'))
+      end
+    end
+
+    context 'While Testing update_confirmed_at Method/Activerecord::Callbacks' do
+      it "Should Return 'true' After Update" do
+        expect(user.confirmed_at).not_to(be_falsey)
+      end
     end
   end
 end
