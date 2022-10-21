2c6518474b4d78585ed2f2bd3dd4b8b23371aa08
Artur Masagutov <masagutovartur765@gmail.com>
2022-10-21 16:30:09 +0500

    Added admin values to Struct Hash

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,25 @@
 # frozen_string_literal: true
 
-first_name = 'Autr'
-password   = 'Test@123'
-last_name  = 'Mesagutov'
-phone      = '2012987481'
-token      = SecureRandom.hex(10)
+admin = OpenStruct.new(
+  f_name: 'Artur',
+  l_name: 'Masagutov',
+  phone: '2012987481',
+  password: 'Test@123',
+  email: 'artur@ipm.com',
+  token: SecureRandom.hex(10),
+  uuid: SecureRandom.uuid,
+  date: DateTime.now
+)
 
-CreateAdminService.call(first_name, last_name, 'autr@ipm.com', password, token, DateTime.now, phone, SecureRandom.uuid)
-
-CreateAdminService.call(first_name, last_name, 'artur@ipm.com', password, token, DateTime.now, phone, SecureRandom.uuid)
+CreateAdminService.call(admin.f_name,
+                        admin.l_name,
+                        admin.email,
+                        admin.password,
+                        admin.token,
+                        admin.date,
+                        admin.phone,
+                        admin.uuid
+                       )
 
 Rake::Task['zipcodes:update'].invoke
 
