671036bd847667636aa480aaf262d02d6d559759
Artur Masagutov <masagutovartur765@gmail.com>
2022-10-25 14:07:52 +0500

    Implement Rubocop fixes for account visitors helper

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module SubscriptionsHelper
-
   def process_plan(user)
     plan_heading(user) if user.subscription.present?
   end
@@ -9,18 +8,14 @@ module SubscriptionsHelper
   private
 
   def plan_heading(user)
-    case user.subscription&.plan
-    when 'free'
-      { name: 'Free Account', description: free_description }
-    when 'premium_trail'
-      { name: 'Trial', description: }
-    when 'premium_monthly'
-      { name: 'Monthly Plan', description: }
-    when 'premium_annual'
-      { name: 'Annual Plan', description: }
-    else
-      ''
-    end
+    subscription_plans = {
+      'premium_trail' => { name: 'Trial', description: },
+      'premium_annual' => { name: 'Annual Plan', description: },
+      'premium_monthly' => { name: 'Monthly Plan', description: },
+      'free' => { name: 'Free Account', description: free_description }
+    }
+
+    subscription_plans[user.subscription&.plan]
   end
 
   def free_description
@@ -32,5 +27,4 @@ module SubscriptionsHelper
     'Unlimited access to our legal document library, online document storage and digital
      signing feature.'
   end
-
 end
--- app/helpers/visitors_helper.rb
+++ app/helpers/visitors_helper.rb
@@ -1,52 +1,62 @@
 # frozen_string_literal: true
 
 module VisitorsHelper
-
   def visitor_card_image(index)
     if index.zero?
-      image_tag 'visitors/my_doc_icon.svg',
+      image_tag('visitors/my_doc_icon.svg',
                 class: 'card_image pull-left'
-      #elsif index == 1
-      #image_tag 'visitors/rental_app_icon.svg',
-      #class: 'card_image pull-left'
+               )
+      # elsif index == 1
+      # image_tag 'visitors/rental_app_icon.svg',
+      # class: 'card_image pull-left'
     elsif index == 1
-      image_tag 'visitors/my_prop_icon.svg',
+      image_tag('visitors/my_prop_icon.svg',
                 class: 'card_image pull-left'
+               )
     elsif index == 2
-      image_tag 'visitors/acc_set_icon.svg',
+      image_tag('visitors/acc_set_icon.svg',
                 class: 'card_image pull-left'
+               )
+    else
+      ''
     end
   end
 
   def visitor_card_heading(index)
     if index.zero?
       'My Documents'
-      #elsif index == 1
-      #'Rental Applications'
+      # elsif index == 1
+      # 'Rental Applications'
     elsif index == 1
       'My Properties'
     elsif index == 2
       'Account Settings'
+    else
+      ''
     end
   end
 
   def visitor_card_description(index)
     if index.zero?
       'Create, sign and manage lease agreements, notice forms and other documents.'
-      #elsif index == 1
-      #'Easily screen tenants with our online rental application form.'
+      # elsif index == 1
+      # 'Easily screen tenants with our online rental application form.'
     elsif index == 1
       'Save and review information about your rental properties and tenants.'
     elsif index == 2
       'view and edit login, contact info and subscription details.'
+    else
+      ''
     end
   end
 
   def visitor_card_arrow(index)
     if index.zero?
-      image_tag image_path
+      image_tag(image_path)
     elsif [1, 2].include?(index)
-      image_tag 'visitors/inactive_arrow.svg'
+      image_tag('visitors/inactive_arrow.svg')
+    else
+      index
     end
   end
 
@@ -59,14 +69,12 @@ module VisitorsHelper
   end
 
   def page_redirection(index)
-    case index
-    when 0
-      documents_path
-    when 1
-      properties_path
-    when 2
-      account_path
-    end
-  end
+    redirections = {
+      0 => documents_path,
+      1 => properties_path,
+      2 => account_path
+    }
 
+    redirections[index]
+  end
 end
