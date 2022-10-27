17256bf4da5ca05efbbbc68cc3fc77771869455d
Artur Masagutov <masagutovartur765@gmail.com>
2022-10-27 12:45:45 +0500

    Implement Rubocop fixes for Token Model

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,8 +1,8 @@
-class Token < ApplicationRecord
+# frozen_string_literal: true
 
+class Token < ApplicationRecord
   def self.create_access_token
     resp = Rocket::GenerateAccessTokenService.call
     return create(access_token: resp['access_token']) unless resp.nil?
   end
-
 end
