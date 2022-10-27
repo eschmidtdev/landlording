909bf12441d607b4a8b011f07e81d27d4331b63e
Artur Masagutov <masagutovartur765@gmail.com>
2022-10-27 14:19:50 +0500

    Implement Rubocop fixes for GenerateAccessTokenService

--- app/services/rocket/generate_access_token_service.rb
+++ app/services/rocket/generate_access_token_service.rb
@@ -2,11 +2,6 @@
 
 module Rocket
   class GenerateAccessTokenService < ApplicationService
-
-    def initialize
-      super
-    end
-
     def call
       generate_access_token
     end
@@ -22,16 +17,16 @@ module Rocket
       request = Net::HTTP::Post.new(url)
       request['Content-Type'] = 'application/json'
       request.body = JSON.dump({
-                                 client_id: ENV.fetch('CLIENT_ID', nil),
-                                 client_secret: ENV.fetch('CLIENT_SECRET', nil),
-                                 grant_type: 'client_credentials'
-                               })
+        client_id: ENV.fetch('CLIENT_ID', nil),
+        client_secret: ENV.fetch('CLIENT_SECRET', nil),
+        grant_type: 'client_credentials'
+      }
+                              )
 
       response = https.request(request)
       return nil unless response.code == '200'
 
       JSON.parse(response.read_body)
     end
-
   end
 end
