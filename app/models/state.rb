7f598d6fcf296bf29f942e5bfbb8c572ed9f3425
Artur Masagutov <masagutovartur765@gmail.com>
2022-10-27 12:43:31 +0500

    Implement Rubocop fixes for County Model

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,15 +1,24 @@
+# frozen_string_literal: true
+
 require 'memoist'
 
-class State < ActiveRecord::Base
+class State < ApplicationRecord
   extend Memoist
-  has_many :zipcodes
-  has_many :counties
 
+  # Associations
+  has_many :zipcodes, dependent: :destroy
+  has_many :counties, dependent: :destroy
+
+  # Validations
+
+  # rubocop: disable Rails/UniqueValidationWithoutIndex
   validates :abbr, uniqueness: { case_sensitive: false }, presence: true
   validates :name, uniqueness: { case_sensitive: false }, presence: true
+  # rubocop: enable Rails/UniqueValidationWithoutIndex
 
   def cities
     zipcodes.map(&:city).sort.uniq
   end
+
   memoize :cities
 end
