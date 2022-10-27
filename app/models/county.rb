2ac26e9efefce348ace6d067d3b64d83432c7253
Artur Masagutov <masagutovartur765@gmail.com>
2022-10-27 12:25:42 +0500

    Implement Rubocop fixes for County Model

--- app/models/county.rb
+++ app/models/county.rb
@@ -1,20 +1,29 @@
+# frozen_string_literal: true
+
 require 'memoist'
 
-class County < ActiveRecord::Base
-  extend Memoist
+class County < ApplicationRecord
+  extend Memxist
+
+  # Associations
   belongs_to :state
-  has_many :zipcodes
 
-  validates :name, uniqueness: { scope: :state_id, case_sensitive: false },
-                   presence: true
+  has_many :zipcodes, dependent: :destroy
 
+  # Validations
+
+  # rubocop: disable Rails/UniqueValidationWithoutIndex
+  validates :name, uniqueness: { scope: :state_id, case_sensitive: false }, presence: true
+  # rubocop: enable Rails/UniqueValidationWithoutIndex
+
+  # Scopes
   scope :without_zipcodes, lambda {
     county = County.arel_table
     zipcodes = Zipcode.arel_table
-    zipjoin = county
-              .join(zipcodes, Arel::Nodes::OuterJoin)
-              .on(zipcodes[:county_id].eq(county[:id]))
-    joins(zipjoin.join_sources).where(zipcodes[:county_id].eq(nil))
+    zip_join = county
+               .join(zipcodes, Arel::Nodes::OuterJoin)
+               .on(zipcodes[:county_id].eq(county[:id]))
+    joins(zip_join.join_sources).where(zipcodes[:county_id].eq(nil))
   }
   scope :without_state, lambda {
     where(state_id: nil)
@@ -23,5 +32,6 @@ class County < ActiveRecord::Base
   def cities
     zipcodes.map(&:city).sort.uniq
   end
+
   memoize :cities
 end
