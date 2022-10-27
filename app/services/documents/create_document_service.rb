2aa098acf23fbedf973bcb58cd2fe45515c970d0
Artur Masagutov <masagutovartur765@gmail.com>
2022-10-27 13:51:45 +0500

    Implement Rubocop fixes for CreateDocumentService

--- app/services/documents/create_document_service.rb
+++ app/services/documents/create_document_service.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 module Documents
   class CreateDocumentService < ApplicationService
-    attr_reader :interview_id, :user
+    attr_accessor :interview_id, :user
 
     def initialize(interview_id, user)
-      @user = user
+      @user         = user
       @interview_id = interview_id
     end
 
@@ -14,9 +16,8 @@ module Documents
     private
 
     def create_document
-      document = Document.create(name: 'Testing Document', user_id: user.id, interview_id:)
+      document = Document.create!(name: 'Testing Document', user_id: user.id, interview_id:)
       document.attributes
     end
-
   end
 end
