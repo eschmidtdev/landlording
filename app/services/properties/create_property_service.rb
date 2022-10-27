941969b90aae28ec3d4fad66bb29f4578f8dade2
Artur Masagutov <masagutovartur765@gmail.com>
2022-10-27 14:14:27 +0500

    Implement Rubocop fixes for UpdatePropertyService & CreatePropertyService

--- app/services/properties/create_property_service.rb
+++ app/services/properties/create_property_service.rb
@@ -1,34 +1,33 @@
 # frozen_string_literal: true
+
 module Properties
   class CreatePropertyService < ApplicationService
-    attr_reader :property_params, :tenant_params
+    attr_accessor :property_params, :tenant_params
 
     MESSAGES = {
-      created: I18n.t('Properties.Created'),
+      created: I18n.t('Properties.Created')
     }.freeze
+    private_constant :MESSAGES
 
     def initialize(property_params, tenant_params)
-      @tenant_params = tenant_params
+      @tenant_params   = tenant_params
       @property_params = property_params
     end
 
     def call
-      begin
-        ActiveRecord::Base.transaction do
-          property = Property.new(property_params)
-          property.save!
-          tenant = Tenant.new(merged_tenant_params(property.id))
-          tenant.save!
-        end
-        { success: true, message: MESSAGES[:created] }
-      rescue => e
-        { success: false, message: e.message }
+      ActiveRecord::Base.transaction do
+        property = Property.new(property_params)
+        property.save!
+        tenant = Tenant.new(merged_tenant_params(property.id))
+        tenant.save!
       end
+      { success: true, message: MESSAGES[:created] }
+    rescue StandardError => e
+      { success: false, message: e.message }
     end
 
     private
 
     def merged_tenant_params(property_id) = tenant_params.merge({ property_id: })
-
   end
 end
--- app/services/properties/update_property_service.rb
+++ app/services/properties/update_property_service.rb
@@ -5,12 +5,13 @@ module Properties
     attr_reader :property, :property_params, :tenant_params
 
     MESSAGES = {
-      updated: I18n.t('Properties.Updated'),
+      updated: I18n.t('Properties.Updated')
     }.freeze
+    private_constant :MESSAGES
 
     def initialize(property, property_params, tenant_params)
-      @property = property
-      @tenant_params = tenant_params
+      @property        = property
+      @tenant_params   = tenant_params
       @property_params = property_params
     end
 
@@ -25,9 +26,11 @@ module Properties
       if property.update(property_params) && tenant.update(tenant_params)
         { success: true, message: MESSAGES[:updated] }
       else
-        { success: false, message: property.errors.full_messages.present? ? property.errors.full_messages.join(', ') : tenant.errors.full_messages.join(', ') }
+        {
+          success: false,
+          message: property.errors.full_messages.present? ? property.errors.full_messages.join(', ') : tenant.errors.full_messages.join(', ')
+        }
       end
     end
-
   end
 end
