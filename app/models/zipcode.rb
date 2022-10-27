7d0c6cc9a02f8e080d0df2565db7f6d85b0dd1ce
Artur Masagutov <masagutovartur765@gmail.com>
2022-10-27 12:49:09 +0500

    Implement Rubocop fixes for Zipcode Model

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -1,25 +1,34 @@
-class Zipcode < ActiveRecord::Base
-  belongs_to :county
-  belongs_to :state
+# frozen_string_literal: true
 
+class Zipcode < ApplicationRecord
+  # Associations
+  belongs_to :state
+  belongs_to :county
+
+  # Validations
+
+  # rubocop: disable Rails/UniqueValidationWithoutIndex
   validates :code, uniqueness: true, presence: true
+  # rubocop: enable Rails/UniqueValidationWithoutIndex
+
   validates :state_id, :county_id, :city, presence: true
 
-  scope :without_county, -> { where("county_id IS NULL") }
-  scope :without_state, -> { where("state_id IS NULL") }
-  scope :ungeocoded, -> { where("lat IS NULL OR lon IS NULL") }
+  # Scopes
+  scope :without_county, -> { where(county_id: nil) }
+  scope :without_state, -> { where(state_id: nil) }
+  scope :ungeocoded, -> { where('lat IS NULL OR lon IS NULL') }
 
   class << self
     def find_by_city_state(city, state)
       includes(county: :state)
-        .where("city like ? AND states.abbr like ?", "#{city}%", "%#{state}%")
+        .where('city like ? AND states.abbr like ?', "#{city}%", "%#{state}%")
         .references(:state)
         .first
     end
 
     def find_all_by_city_state(city, state)
       includes(county: :state)
-        .where("city like ? AND states.abbr like ?", "#{city}%", "%#{state}%")
+        .where('city like ? AND states.abbr like ?', "#{city}%", "%#{state}%")
         .references(:state)
     end
   end
